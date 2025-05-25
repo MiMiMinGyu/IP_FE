0bfc7c2f33c00554e4b41aeea7d8ffd6247e420a
MiMiMinGyu <awingmin@naver.com>
2025-05-25 23:38:24 +0900

    상단바 제작 및 로고 삽입, 로그인 & 회원가입 페이지 제작

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,1 +1,23 @@
-// 웹 상단 바
+import { useNavigate } from 'react-router-dom';
+import everything_logo from '../assets/everything_logo.png';
+import '../styles/Header.css';
+
+const Header = () => {
+    const navigate = useNavigate();
+
+    return (
+        <header className="header-container">
+            <div className="header-logo" onClick={() => navigate('/')}>
+                <img src={everything_logo} alt="everyThing" className="header-logo-image" />
+            </div>
+
+            <nav className="header-nav">
+                <button className="header-search-button"> 🔎검색 </button>
+                <button className="header-login-button" onClick={() => navigate('/login')}> 로그인 </button>
+                <button className="header-mypage-button"> 마이페이지 </button>
+            </nav>
+        </header>
+    );
+};
+
+export default Header;
--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -2,6 +2,8 @@ import { Routes, Route } from 'react-router-dom';
 import Layout from '../components/Layout';
 import Home from '../pages/Home';
 import Intro from '../pages/boards/Intro';
+import Login from '../pages/Login';
+import SignUp from '../pages/SignUp';
 
 function Router() {
     return (
@@ -10,6 +12,10 @@ function Router() {
                 <Route index element={<Home />} />
                 <Route path="/boards/intro" element={<Intro />} />
             </Route>
+
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<SignUp />} />
+            
         </Routes>
     );
 }
