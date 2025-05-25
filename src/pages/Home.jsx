bd3ed395ce5f8950a701777b51c41dd68d4a92b9
MiMiMinGyu <awingmin@naver.com>
2025-05-25 19:57:50 +0900

    레이아웃 제작

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,13 @@
-import '../styles/Home.css';
+import React from 'react';
+import Layout from '../components/Layout';
 
-function Home() {
-    return (
-        <div>
-
-        </div>
-    );
-}
+const Home = () => {
+  return (
+    <Layout>
+      <h1>게시판 홈</h1>
+      <p>여기는 게시판 메인 영역입니다.</p>
+    </Layout>
+  );
+};
 
 export default Home;
