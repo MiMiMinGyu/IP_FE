ef5d784febac083d58661de60b23a0a0a67fd4aa
jungjaehun00 <emit1011@naver.com>
2025-06-02 05:55:54 +0900

    commit test calendar test

--- src/pages/academic-info/Calendar.jsx
+++ src/pages/academic-info/Calendar.jsx
@@ -3,6 +3,7 @@ const Calendar = () => {
     <div className="page-container">
       <h1 className="page-title">수강 신청 일정</h1>
       <p className="page-description">여기에 수강 신청 일정에 대한 설명을 작성하세요.</p>
+      <p>test</p>
     </div>
   );
 };
